f4650b335ca11c6d19ed018ed91fd672d823d00c
asselmankarel <asselman.karel@gmail.com>
2021-06-07 16:31:01 +0200

    DAL Created generic repository interface

---
+++ FleetManagment.DAL/Repositories/GenericRepository.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace FleetManagment.DAL.Repositories
+{
+    class GenericRepository<T> : IGenericRepository<T> where T : class
+    {
+
+        public GenericRepository(Applcation)
+        {
+
+        }
+        public T GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<T> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Add(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Change(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
