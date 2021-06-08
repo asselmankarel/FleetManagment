eff5526ff55a80906994ee582ac21f6a0f521809
asselmankarel <asselman.karel@gmail.com>
2021-06-08 15:19:03 +0200

    WriteAPI

--- FleetManagment.WriteAPI/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace FleetManagment.WriteAPI.Areas.HelpPage.ModelDescriptions
-{
-    public class DictionaryModelDescription : KeyValuePairModelDescription
-    {
-    }
-}
--- FleetManagment.WriteAPI/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace FleetManagment.WriteAPI.Areas.HelpPage.ModelDescriptions
-{
-    public class SimpleTypeModelDescription : ModelDescription
-    {
-    }
-}
