a9287c2257f39fc1c6bb6e20d0f077b843f36cb7
Karel.Asselman <karel.asselman@allphi.eu>
2021-06-11 12:42:01 +0200

    Seeds for Driver and Vehicle

--- FleetManagement.Seed/Program.cs
+++ FleetManagement.Seed/Program.cs
@@ -1,33 +1,45 @@
-﻿using FleetManagement.DAL.DataAccess;
+﻿using Bogus;
+using FleetManagement.DAL.DataAccess;
 using FleetManagement.Domain.Models;
-using System;
 
 namespace FleetManagement.Seed
 {
     class Program
     {
+        private static ApplicationDbContext _context;
+
         static void Main(string[] args)
         {
-            var context = new ApplicationDbContext();
+            _context = new ApplicationDbContext();
 
-            var driver = context.Drivers.Find(1);
-            driver.DriversLicense = DriversLicense.B;
+            //CreateDrivers(10);
+            //CreateVehicles(10);
 
-            //var driver = new Driver()
-            //{
-            //    NIS = "06641070483",
-            //    FirstName = "Karel",
-            //    LastName = "Asselman",
-            //    DateOfBirth = DateTime.Parse("07-04-1983")
-            //};
+            _context.SaveChanges();
+        }
 
-            //context.Drivers.Add(driver);
-            Console.WriteLine(driver.DriversLicense);
+        private static void CreateVehicles(int numberOfVehicles)
+        {
+            var VehicleFaker = new Faker<Vehicle>()
+               .RuleFor(d => d.Vin, f => f.Random.AlphaNumeric(17))
+               .RuleFor(d => d.VehicleType, f => (VehicleType)f.Random.Int(0, 2))
+               .RuleFor(v => v.FuelType, f => (FuelType)f.Random.Int(0, 3));
 
-            context.SaveChanges();
-            
-            Console.WriteLine("End...");
-            Console.ReadLine();
+            var vehicles = VehicleFaker.Generate(numberOfVehicles);
+            _context.Vehicles.AddRange(vehicles);
+        }
+
+        private static void CreateDrivers(int numberOfDrivers)
+        {            
+            var DriverFaker = new Faker<Driver>()
+                .RuleFor(d => d.NIS, f => $"{f.Random.Long(01000000000, 99999999999)}")
+                .RuleFor(d => d.FirstName, f => f.Person.FirstName)
+                .RuleFor(d => d.LastName, f => f.Person.LastName)
+                .RuleFor(d => d.DateOfBirth, f => f.Person.DateOfBirth)
+                .RuleFor(d => d.DriversLicense, f => (DriversLicense)f.Random.Int(0, 4));
+
+            var drivers = DriverFaker.Generate(numberOfDrivers);
+            _context.Drivers.AddRange(drivers);            
         }
     }
 }
