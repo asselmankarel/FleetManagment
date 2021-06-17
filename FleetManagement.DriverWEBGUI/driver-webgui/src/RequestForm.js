c5f5a38b81ed51cd3eb7879804343e84ea50e5ff
Karel.Asselman <karel.asselman@allphi.eu>
2021-06-17 16:33:46 +0200

    DriverWEBGUI: RequestForm behavior

--- FleetManagement.DriverWEBGUI/driver-webgui/src/RequestForm.js
+++ FleetManagement.DriverWEBGUI/driver-webgui/src/RequestForm.js
@@ -1,15 +1,29 @@
-import React from 'react';
+import React, {useState} from 'react';
+import { NavLink } from 'react-router-dom';
+import useFetch from './useFetch';
 import './css/RequestForm.css';
 
 export default function RequestForm() {
 
+    const [ type, setType ] = useState('');
+    const [ date1, setDate1 ] = useState('');
+    const [ date2, setDate2 ] = useState('');
+
+    //const [ post, loading ] = useFetch('https://localhost:44318/');
+ 
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        alert(`submit triggerd ${type}, ${date1}, ${date2}`);
+    }
+
     return(
         <div className="request-form">
-            <form>
+            <form onSubmit={ handleSubmit } >
                 <div className="form-group">                    
                     <label>Type of request</label>               
                     <div className="form-item">
-                        <select>
+                        <select onChange={e => setType(e.target.value)} value={type} required>
+                            <option></option>
                             <option>Fuelcard</option>
                             <option>Maintenance</option>
                             <option>Repair</option>
@@ -21,20 +35,20 @@ export default function RequestForm() {
                 <div className="form-group">
                     <label>Preferred date 1:</label>
                     <div className="form-item">
-                        <input type="date" />
+                        <input type="date" value={date1} onChange={e => setDate1(e.target.value)} required />
                     </div>
                 </div>
 
                 <div className="form-group">
                     <label>Preferred date 2:</label>
                     <div className="form-item">
-                        <input type="date" />
+                        <input type="date" value={date2} onChange={e => setDate2(e.target.value)} />
                     </div>
                 </div>
                 
                 <div className="button-group">
-                    <button className="button">Cancel</button>
-                    <button className="button">Save</button>
+                    <NavLink to="/" ><button className="button button-cancel">Cancel</button></NavLink>
+                    <button className="button">Send request</button>
                 </div>
             </form>
         </div>
