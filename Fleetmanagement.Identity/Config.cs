ded04790d2edd0d0527225fcd9260529dd8e7ba0
Karel.Asselman <karel.asselman@allphi.eu>
2021-07-13 10:33:29 +0200

    Identity setup from scratch with sqlServer DB

--- Fleetmanagement.Identity/Config.cs
+++ Fleetmanagement.Identity/Config.cs
@@ -10,17 +10,37 @@ namespace Fleetmanagement.Identity
     public static class Config
     {
         public static IEnumerable<IdentityResource> IdentityResources =>
-            new IdentityResource[]
-            { 
-                new IdentityResources.OpenId()
-            };
+                   new IdentityResource[]
+                   {
+                new IdentityResources.OpenId(),
+                new IdentityResources.Profile(),
+                   };
 
         public static IEnumerable<ApiScope> ApiScopes =>
             new ApiScope[]
-            { };
+            {
+                new ApiScope("fleetmanagement-read-api", "Fleetmanagement read API"),
+                new ApiScope("fleetmanagement-write-api", "Fleetmanagement write API"),
+            };
 
         public static IEnumerable<Client> Clients =>
-            new Client[] 
-            { };
+            new Client[]
+            {
+                // interactive client using code flow + pkce
+                new Client
+                {
+                    ClientId = "fleetmanagement-driver-webgui",
+                    ClientSecrets = { new Secret("49C1A7E1-0C79-4A89-A3D6-A37998FB86B0".Sha256()) },
+
+                    AllowedGrantTypes = GrantTypes.Code,
+
+                    RedirectUris = { "https://localhost:44300/signin-oidc" },
+                    FrontChannelLogoutUri = "https://localhost:44300/signout-oidc",
+                    PostLogoutRedirectUris = { "https://localhost:44300/signout-callback-oidc" },
+
+                    AllowOfflineAccess = true,
+                    AllowedScopes = { "openid", "profile", "scope2" }
+                },
+            };
     }
 }
